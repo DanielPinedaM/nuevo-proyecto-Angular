516b949572db6e4fd5b27fe7ac6a3e8494427f72
daniel <daniel.pinedamo@amigo.edu.co>
2025-03-10 21:26:30 -0500

    feat: :sparkles: mejorar appConfig
    mejorar appConfig
    
    mejorar appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 import Aura from '@primeng/themes/aura';
-import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    provideHotToastConfig(),
 
     // Prime NG
     provideAnimationsAsync(),
