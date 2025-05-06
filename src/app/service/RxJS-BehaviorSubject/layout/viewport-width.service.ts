2f55868dbca84e6c7b3e31bd92a9c0e12a524212
daniel <daniel.pinedamo@amigo.edu.co>
2025-05-05 21:40:37 -0500

    feat: :sparkles: crear RxJS Behavior Subjext q devuelve un numero con el ancho del viewport (pantalla)
    crear RxJS Behavior Subjext q devuelve un numero con el ancho del viewport (pantalla)
    
    crear RxJS Behavior Subjext q devuelve un numero con el ancho del viewport (pantalla)

---
+++ src/app/service/RxJS-BehaviorSubject/layout/viewport-width.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+/**
+devuelve un numero con el ancho del viewport (pantalla),
+se ejecuta cada vez q cambia el ancho de la pantalla,
+Ejemplo: al girar el celular */
+export class ViewportWidthService {
+  private viewportWidthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(window?.innerWidth);
+  public viewportWidth$: Observable<number> = this.viewportWidthSubject.asObservable();
+
+  getViewportWidth = (): Observable<number> => this.viewportWidth$;
+
+  setViewportWidth(viewportWidth: number): void {
+    if (this.viewportWidthSubject.value !== viewportWidth) {
+      this.viewportWidthSubject.next(viewportWidth);
+    }
+  }
+}
