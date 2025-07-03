3abab03cb3783df8ba116dc6cf9003da339aef0a
daniel <daniel.pinedamo@amigo.edu.co>
2025-07-02 23:33:43 -0500

    feat: :recycle: mejorar funcion isBoolean
    mejorar funcion isBoolean
    
    mejorar funcion isBoolean

--- src/app/utils/class/DataTypeClass.ts
+++ src/app/utils/class/DataTypeClass.ts
@@ -128,16 +128,15 @@ Ejemplo: "-1,2.1", "-2", "3" */
   };
 
   public static isBoolean = (variable: boolean | string | any): boolean => {
-    const normalized: string = String(variable)?.trim()?.toLowerCase();
-
     if (
       // true
-      normalized === 'true' ||
-      normalized === '1' ||
+      DataTypeClass.normalizeStr(variable) === 'true' ||
+      DataTypeClass.normalizeStr(variable) === '1' ||
       DataTypeClass.normalizeStr(variable) === 'si' ||
+      DataTypeClass.normalizeStr(variable) === 'yes' ||
       // false
-      normalized === 'false' ||
-      normalized === '0' ||
+      DataTypeClass.normalizeStr(variable) === 'false' ||
+      DataTypeClass.normalizeStr(variable) === '0' ||
       DataTypeClass.normalizeStr(variable) === 'no'
     ) {
       return true;
