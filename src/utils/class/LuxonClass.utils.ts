c21d5f71cf257acd3e2df4dbbb3b2737b4ac032e
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2025-08-19 15:31:42 -0500

    feat: :recycle: meejorar metodo para formato de fecha y/o hora con formato personalizado
    meejorar metodo para formato de fecha y/o hora con formato personalizado
    
    meejorar metodo para formato de fecha y/o hora con formato personalizado

--- src/utils/class/LuxonClass.utils.ts
+++ src/utils/class/LuxonClass.utils.ts
@@ -3,7 +3,7 @@ import { Nullable } from 'primeng/ts-helpers';
 
 export default class LuxonClass {
   /**
-   Eliminar espacio en blanco reemplazando:
+  Eliminar espacio en blanco reemplazando:
   - "p. m" por "p.m"
   - "a. m" por "a.m" */
   public static replaceAmPm = (date: Date | string | any): string | any => {
@@ -16,30 +16,7 @@ export default class LuxonClass {
   };
 
   /**
-  formato de hora */
-  public static formatHour = (
-    date: Date | string | Nullable<Date> | any,
-    format: string = 'd-LLL-yyyy'
-  ): string | any => {
-    let dateTime: DateTime;
-
-    if (date instanceof Date) {
-      dateTime = DateTime.fromJSDate(date);
-    } else if (typeof date === 'string' && String(date)?.trim() !== '') {
-      dateTime = DateTime.fromISO(date);
-    } else {
-      return date;
-    }
-
-    if (!dateTime.isValid) return date;
-
-    const finalDate: string = dateTime.setLocale('es').toFormat(format);
-
-    return this.replaceAmPm(finalDate);
-  };
-
-  /**
-  formato de fecha */
+  formato de fecha y/o hora con formato personalizado */
   public static formatDate = (
     date: Date | string | Nullable<Date> | DateTime,
     format: string = 'd-LLL-yyyy'
@@ -59,29 +36,6 @@ export default class LuxonClass {
     if (!dateTime.isValid) return date;
 
     return dateTime.setLocale('es').toFormat(format);
-  };
-
-  /**
-  formato de fecha y hora con a.m y p.m */
-  public static dateAndTimeFormat = (
-    date: Date | string | Nullable<Date>,
-    format: string = 'd-LLL-yyyy hh:mm a'
-  ): string | any => {
-    let dateTime: DateTime;
-
-    if (date instanceof Date) {
-      dateTime = DateTime.fromJSDate(date);
-    } else if (typeof date === 'string' && String(date)?.trim() !== '') {
-      dateTime = DateTime.fromISO(date);
-    } else {
-      return date;
-    }
-
-    if (!dateTime.isValid) return date;
-
-    const finalDate: string = dateTime.setLocale('es').toFormat(format);
-
-    return this.replaceAmPm(finalDate);
   };
 
   /**
