346c9afa6ce3fddd7774344bd183e2810058700e
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-05-19 21:13:41 -0500

    fix: :bug: agregar carpeta api

---
+++ src/shared/api/general-api/types/request-data.types.ts
@@ -0,0 +1,73 @@
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+
+/**
+nombres de los metodos HTTP */
+export type TMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+/**
+tipo de respuesta HTTP */
+export type IResponseType = 'arraybuffer' | 'blob' | 'json' | 'text';
+
+/**
+type de query params */
+type TParams =
+  | HttpParams
+  | Record<
+      string,
+      string | number | boolean | readonly (string | number | boolean)[]
+    >
+  | undefined;
+/**
+type de headers */
+type THeaders = HttpHeaders | Record<string, string | string[]> | undefined;
+
+export interface IRequestOptions<T = any> {
+  body?: T;
+  params?: TParams;
+  headers?: THeaders;
+  responseType?: IResponseType;
+  showLoader?: boolean;
+  showLogger?: boolean;
+  executeErrorHandling?: boolean;
+
+  isASecurityEndpoint?: boolean;
+  withCredentials?: boolean;
+}
+
+/**
+asi es como responde la API */
+export interface IResponse<T = any> {
+  success: boolean;
+  status: number;
+  message: string;
+  data: T;
+}
+
+export type ResponseType = IResponse | Blob | FormData | any;
+
+/**
+parametros de funcion errorLogs() q se imprimen por consola  cuando hay errores */
+export interface IObjectLogs {
+  method?: TMethod;
+  url?: string;
+  options?: IRequestOptions;
+  response: IResponse;
+  showLogger: boolean;
+}
+
+/**
+este es el formato (tipo de dato) q Angular le da a los errores */
+export interface IHttpErrorResponse {
+  headers: {
+    headers: any;
+    normalizedNames: any;
+    lazyUpdate: any;
+  };
+  status: number;
+  statusText: string;
+  url: string;
+  ok: boolean;
+  name: string;
+  message: string;
+  error: IResponse;
+}
---
+++ src/shared/api/general-api/utils/request-data.utils.ts
@@ -0,0 +1,269 @@
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '@/environments/environment';
+import {
+  IObjectLogs,
+  IRequestOptions,
+  IResponse,
+} from '@/shared/api/general-api/types/request-data.types';
+import { HttpHeaders } from '@angular/common/http';
+import HotToastClass from '@/shared/utils/class/notification/HotToastClass.utils';
+import { LoaderService } from '@/shared/service/RxJS-BehaviorSubject/layout/loader.service';
+import DataTypeClass from '@/shared/utils/class/DataTypeClass.utils';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestDataUtils {
+  dataTypeClass = inject(DataTypeClass);
+  private hotToast = inject(HotToastClass);
+  private router = inject(Router);
+  private loaderService = inject(LoaderService);
+
+  unauthorized(): void {
+    this.router.navigate(['/iniciar-sesion']);
+    this.loaderService.setLoader(false);
+  }
+
+  redirectToLogin(): void {
+    const login: string = '/iniciar-sesion';
+
+    if (!this.pathnameIsLogin()) {
+      window.location.href = login;
+    }
+  }
+
+  returnToBrowserHistory(): void {
+    if (window.history.length > 1) {
+      window.history.go(-1);
+    } else {
+      this.redirectToLogin();
+    }
+  }
+
+  pathnameIsLogin(): boolean {
+    const login: string = '/iniciar-sesion';
+
+    const pathname: string = window.location.pathname;
+
+    return pathname === login;
+  }
+
+  /**
+  manejo de errores */
+  errorHandling(status: number | undefined, url: string): void {
+    if (!status) return;
+
+    if (status === 401) {
+      console.error(
+        '❌ http.service.ts - Error 401: unauthenticated',
+        '\nDetalle: El usuario no está autenticado o la sesión ha expirado',
+        '\nAcción: Re-dirigir al usuario a la página de inicio de sesión',
+        '\nURL solicitada: ',
+        url
+      );
+
+      // re-dirigir a /iniciar-sesion cuando el status de la respuesta de la api sea 401
+      this.unauthorized();
+
+      if (!this.pathnameIsLogin()) {
+        this.hotToast.infoNotification('Inicie sesión para continuar');
+      }
+    } else if (status === 403) {
+      console.error(
+        '❌ http.service.ts - Error 403: Forbidden',
+        '\nDetalle: El usuario está autenticado pero no tiene permisos para acceder al recurso',
+        "\nAcción: Mostrar un mensaje de 'acceso denegado' o re-dirigir y re-dirigir a la pagina web anterior en el historial del navegador",
+        '\nURL solicitada:',
+        url
+      );
+
+      // devolverme a la web anterior en el historial cuando el status de la respuesta de la api sea 403
+      this.returnToBrowserHistory();
+
+      this.hotToast.infoNotification(
+        'Acceso denegado, no tiene permisos para realizar esta acción'
+      );
+    } else if (status === 404) {
+      console.error(
+        `❌ http.service.ts - error 404: Not Found - endpoint no encontrado, la URL solicitada "${url}" NO existe en el servidor`
+      );
+
+      this.hotToast.errorNotification(
+        'Ha ocurrido un error, por favor comuniquese con el administrador del sistema'
+      );
+    } else if (status >= 500) {
+      console.error(
+        `❌ http.service.ts - error en el servidor en la URL ${url}`
+      );
+
+      this.hotToast.errorNotification(
+        'Ha ocurrido un error, intentalo de nuevo mas tarde, estamos trabajando para solucionarlo'
+      );
+    }
+  }
+
+  /**
+  ✅ imprime logs de cuando la peticion es exitosa */
+  successLogs(objectLogs: IObjectLogs): void {
+    // NO imprimir logs en produccion
+    if (environment.production) return;
+
+    const { method, url, options, response, showLogger } = objectLogs;
+
+    if (!showLogger) return;
+
+    console.info(`✅ [${method}] ${url}`);
+
+    if (this.dataTypeClass.isFile(options?.body)) {
+      console.info('✅ archivo(s) subido(s)');
+    }
+
+    if (options?.body) {
+      console.info('body ', options?.body);
+    }
+
+    if (response) {
+      let { success, status, message, data } = response;
+
+      if (data) {
+        if (Array.isArray(data)) {
+          if (data.length === 0) {
+            data = 'array vacío ➡️ (0) []';
+          } else {
+            // data es un array de objetos [{}]
+            const areAllObjects: boolean = data?.every(
+              (item) => typeof item === 'object' && item && item !== null
+            );
+            if (areAllObjects) {
+              data = `array de objetos con ${data.length} elemento ➡️ (${data.length}) [{}]`;
+            } else {
+              // data es un array []
+              data = `array de ${data.length} elementos ➡️ (${data.length}) []`;
+            }
+          }
+        } else if (this.dataTypeClass.isLiteralObject(data)) {
+          // data es un objeto literal {}
+          const length: number | null =
+            this.dataTypeClass.literalObjectLength(data);
+
+          if (length === 0) {
+            data = 'objeto literal vacío ➡️ (0) {}';
+          } else {
+            data = `objeto literal con ${length} keys ➡️ (${length}) {}`;
+          }
+        }
+      }
+
+      const objectSuccesResponse: IResponse = {
+        success,
+        status,
+        message,
+        data,
+      };
+
+      console.info(
+        `respuesta de la API apuntando a ➡️ ${environment.NODE_ENV} ⬅️`,
+        objectSuccesResponse
+      );
+    }
+
+    console.info('\n');
+  }
+
+  /**
+  ❌ imprime logs de cuando la peticion da error */
+  errorLogs(objectLogs: IObjectLogs): void {
+    // NO imprimir logs en produccion
+    if (environment.production) return;
+
+    const { method, url, options, response, showLogger } = objectLogs;
+
+    if (!showLogger) return;
+
+    console.error('❌ error ');
+    if (method) console.error('metodo HTTP', method);
+    if (url) console.error('url ', url);
+    if (environment.NODE_ENV)
+      console.error(
+        `las variables de entorno estan apuntando al ambiente de ➡️ ${environment.NODE_ENV} ⬅️`
+      );
+    if (options) console.error('options ', options);
+    if (response) console.error('response ', response);
+
+    console.info('\n');
+  }
+
+  /**
+  validar env en los q por defecto NO se incluye el token */
+  defaultSecurityEndpoint(url: string): boolean {
+    // environments con las URL de los endpoints a los q NO se les envia el token al hacer peticion HTTP
+    const unprotectedURLs: string[] = [
+      environment.auth.login,
+      environment.auth.register,
+      environment.auth.recoverPassword,
+      environment.auth.assignPassword,
+    ] as string[];
+
+    for (const item of unprotectedURLs) {
+      if (!this.dataTypeClass.isString(item)) {
+        console.error(
+          `❌ ERROR CRÍTICO\n verifica q el env ${item} este agregado a las variables de entorno \n unprotectedURLs`,
+          unprotectedURLs
+        );
+        return false;
+      }
+    }
+
+    // validar env en los q NO se incluye en token
+    return !unprotectedURLs.some((item: string) => url === item);
+  }
+
+  /**
+  configuraciones por defecto para llamar la API */
+  #DEFAULT_OPTIONS(url: string): IRequestOptions {
+    return {
+      params: {},
+      headers: { 'Content-Type': 'application/json' },
+      responseType: 'json',
+
+      // ¿mostrar icono de cargando?
+      showLoader: true,
+
+      // ¿mostrar logs en consola?
+      showLogger: Boolean(environment.NODE_ENV !== 'production'),
+
+      // ¿ejecutar funcion para manejo global de errores HTTP?
+      executeErrorHandling: true,
+
+      // enviar token en TODOS los endpoint, EXCEPTO los q estan en const unprotectedURLs: string[]
+      //isASecurityEndpoint: this.defaultSecurityEndpoint(url),
+      withCredentials: this.defaultSecurityEndpoint(url),
+    };
+  }
+
+  /**
+  opciones q recibe Angular httpClient */
+  buildHttpOptions(url: string, options: IRequestOptions = {}): any {
+    // Combinar opciones por defecto con las personalizadas
+    const defaultOptions = this.#DEFAULT_OPTIONS(url);
+
+    const mergedOptions: IRequestOptions = {
+      ...defaultOptions,
+      ...options,
+      headers: { ...defaultOptions?.headers, ...options?.headers },
+    };
+
+    const { params, headers, responseType, withCredentials } = mergedOptions;
+
+    return {
+      // devolver solo el contenido de la respuesta de la API, SIN headers NI status NI body
+      observe: 'body' as const,
+
+      headers: new HttpHeaders(headers as Record<string, string | string[]>),
+      params,
+      responseType,
+      withCredentials,
+    };
+  }
+}
